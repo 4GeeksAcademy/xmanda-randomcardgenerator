5adbecd513d9abdeb3eab6b076e29ff0c9c938a3
Amanda Esquivel <128640315+xmanda@users.noreply.github.com>
2023-06-29 00:52:18 +0000

    final changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function newCard() {
   // arrays del contenido que necesitamos en las funciones
   const symbols = ["♦", "♥", "♠", "♣"];
   const numbers = [
@@ -24,32 +24,28 @@ window.onload = function() {
     "K"
   ];
   // función del random card
-  function cardGenerator() {
-    //agarrar clases del html
-    const cardTopElement = document.querySelector(".top");
-    const cardNumberElement = document.querySelector(".number");
-    const cardBottomElement = document.querySelector(".bottom");
+  //agarrar clases del html
+  const cardTopElement = document.querySelector(".top");
+  const cardNumberElement = document.querySelector(".number");
+  const cardBottomElement = document.querySelector(".bottom");
 
-    //randomize
-    const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
-    const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  //randomize
+  const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+  const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
 
-    //insertarse en el html
-    cardTopElement.innerHTML = randomSymbols;
-    cardNumberElement.innerHTML = randomNumbers;
-    cardBottomElement.innerHTML = randomSymbols;
+  //insertarse en el html
+  cardTopElement.innerHTML = randomSymbols;
+  cardNumberElement.innerHTML = randomNumbers;
+  cardBottomElement.innerHTML = randomSymbols;
 
-    // color rojo cuando salen diamantes y corazones
-    if (randomSymbols === "♦" || randomSymbols === "♥") {
-      cardTopElement.classList.add("red");
-      cardNumberElement.classList.add("red");
-      cardBottomElement.classList.add("red");
-    } else {
-      cardTopElement.classList.remove("red");
-      cardNumberElement.classList.remove("red");
-      cardBottomElement.classList.remove("red");
-    }
+  // color rojo (traido desde el css) cuando salen diamantes y corazones
+  if (randomSymbols === "♦" || randomSymbols === "♥") {
+    cardTopElement.classList.add("red");
+    cardNumberElement.classList.add("red");
+    cardBottomElement.classList.add("red");
+  } else {
+    cardTopElement.classList.remove("red");
+    cardNumberElement.classList.remove("red");
+    cardBottomElement.classList.remove("red");
   }
-  //llamar la función
-  cardGenerator();
 };
